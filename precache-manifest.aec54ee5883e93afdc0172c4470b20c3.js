898a2ef362430f30845ae0fa5fc7b8a9412e7769
Sudhanshu Gautam <me@sudhanshug.com>
2019-06-26 02:35:29 +0530

    Updates

---
+++ precache-manifest.aec54ee5883e93afdc0172c4470b20c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b90d3e7fd34e4e45d652e54c2a3d82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "186ffef0add040e507e9",
+    "url": "/static/css/main.9b23e39b.chunk.css"
+  },
+  {
+    "revision": "08dd3be8e3bfaf1e3dc9",
+    "url": "/static/js/2.3cc4f190.chunk.js"
+  },
+  {
+    "revision": "186ffef0add040e507e9",
+    "url": "/static/js/main.7fa2b3cd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
